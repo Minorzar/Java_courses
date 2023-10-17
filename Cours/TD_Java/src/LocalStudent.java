845914e26f1fe041206c7fefa9ea3997c526fd74
Minorzar <gauthier_biehler@yahoo.com>
2023-10-17 11:00:55 +0200

    Update of the architecture of the git. Old code is now on labs branch, main and dev are the ones to be work on

--- Cours/TD_Java/src/LocalStudent.java
+++
@@ -1,7 +0,0 @@
-public class LocalStudent extends  Student{
-
-    public LocalStudent(String firstName, String lastName, String promoName){
-        super(firstName, lastName, promoName) ;
-        this.yearValidation = this.validation() ;
-    }
-}
