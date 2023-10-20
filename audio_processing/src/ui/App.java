c712bd99d6b46161b8f3207ddc1bfef5a5caaa03
Minorzar <gauthier_biehler@yahoo.com>
2023-10-20 18:10:24 +0200

    Correction of the App class and creation of other packages needed inside the project.

--- audio_processing/src/ui/App.java
+++ audio_processing/src/ui/App.java
@@ -1,18 +1,20 @@
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Separator;
-import javafx.scene.control.ToolBar;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
+package ui ;
+
+import javafx.application.Application ;
+import javafx.scene.Group ;
+import javafx.scene.Scene ;
+import javafx.scene.control.ComboBox ;
+import javafx.scene.control.Separator ;
+import javafx.scene.control.ToolBar ;
+import javafx.scene.layout.BorderPane ;
+import javafx.scene.layout.HBox ;
+import javafx.stage.Stage ;
 import javafx.scene.Node ;
 import javafx.scene.control.Label ;
 import javafx.scene.control.TextField ;
+import javafx.scene.control.Button ;
 
-import java.awt.*;
-import java.net.MalformedURLException;
+import java.net.MalformedURLException ;
 
 
 public class App extends Application {
@@ -40,13 +42,13 @@ public class App extends Application {
         cb.getItems().addAll("Item 1", "Item 2", "Item 3");
         Label label = new Label("This is a label");
 
-        button.addActionListener(event -> System.out.println("Button pressed!"));
+        button.setOnAction(event -> System.out.println("Button pressed!"));
         cb.setOnAction(event -> {
             String selected = cb.getSelectionModel().getSelectedItem();
             System.out.println("Selected item: " + selected);
         });
 
-        ToolBar tb = new ToolBar(/*button, */ label, new Separator(), cb);
+        ToolBar tb = new ToolBar(button, label, new Separator(), cb);
         return tb;
     }
 
